a8874bfb110b918be3a8441fa216ba0ee8a6cab6
pip3alfar0 <alfaro.feli@gmail.com>
2024-07-12 19:12:12 -0400

    using namespace std

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+using namespace std;
 
 // Definir las posibles combinaciones de N y p
-std::vector<int> N_values = {1024, 4096, 194481, 65536};
-std::vector<double> p_values = {0, 0.25, 0.5, 0.75, 1.0};
+vector<int> N_values = {1024, 4096, 194481, 65536};
+vector<double> p_values = {0, 0.25, 0.5, 0.75, 1.0};
 
 struct Combination {
     int N;
     double p;
 };
 
-std::vector<Combination> generateCombinations(const std::vector<int>& N_values,
-                                              const std::vector<double>& p_values) {
-    std::vector<Combination> combinations;
+vector<Combination> generateCombinations(const vector<int>& N_values,
+                                              const vector<double>& p_values) {
+    vector<Combination> combinations;
     for (int N : N_values) {
         for (double p : p_values) {
             combinations.push_back({N, p});
@@ -23,11 +24,11 @@ std::vector<Combination> generateCombinations(const std::vector<int>& N_values,
 }
 
 int main() {
-    std::vector<Combination> combinations = generateCombinations(N_values, p_values);
+    vector<Combination> combinations = generateCombinations(N_values, p_values);
 
     // Imprimir las combinaciones
     for (const auto& combination : combinations) {
-        std::cout << "N: " << combination.N << ", p: " << combination.p << std::endl;
+        cout << "N: " << combination.N << ", p: " << combination.p << endl;
     }
 
     return 0;
